4c56edc6389487b4d6843037b75100c2f4c4561f
mounika solleti <mounika solleti@mounikasolleti>
2017-09-05 00:44:25 +0530

    first commit

---
+++ src/qsp/SampleTest.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+	@Test
+	public void sample()
+	{
+		Reporter.log("hiii",true);
+	}
+
+}
